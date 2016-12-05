9c988db20bc233a59ac817d5ddef207f00ffa98c
Jovani Warguez <warguezjovani@yahoo.com>
2016-12-05 16:45:43 +0800

    Add Error Message Box

--- Assignment1/Form3-Number2.vb
+++ Assignment1/Form3-Number2.vb
@@ -1,8 +1,19 @@
 ﻿Public Class FormNumber2
 
     Private Sub ComputeButton_Click(sender As Object, e As EventArgs) Handles SubmitButton.Click
-        MessageBox.Show("Hello " + FirstNameTextBox.Text + " " + LastNameTextBox.Text & vbNewLine & "Your Student Number is " + StudentNumberTextBox.Text, "Greetings!",
-            MessageBoxButtons.OK, MessageBoxIcon.None)
+        Dim firstname As String
+        Dim lastname As String
+        Dim studentnumber As String
+
+        If FirstNameTextBox.Text = "" Or LastNameTextBox.Text = "" Or StudentNumberTextBox.Text = "" Then
+            MessageBox.Show("Please Fill Out The Form",
+                "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            MessageBox.Show("Hello " + FirstNameTextBox.Text + " " + LastNameTextBox.Text & vbNewLine & "Your Student Number is " + StudentNumberTextBox.Text,
+                "Greetings!", MessageBoxButtons.OK, MessageBoxIcon.None)
+        End If
+
+
     End Sub
 
     Private Sub BackButton_Click(sender As Object, e As EventArgs) Handles BackButton.Click
